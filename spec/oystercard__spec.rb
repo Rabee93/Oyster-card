983d2336496bce2f9e8de18de8e416492b760c51
EC2 Default User <ec2-user@ip-172-31-28-29.eu-west-2.compute.internal>
2020-06-16 14:28:10 +0000

    finished step 6

--- spec/oystercard__spec.rb
+++ spec/oystercard__spec.rb
@@ -7,4 +7,17 @@ describe Oystercard do
         expect(oystercard.balance).to eq(0)
        end 
     end 
-end 
+    describe '#top_up' do 
+      it 'adds money to the balance' do
+        oystercard = Oystercard.new
+        oystercard.top_up(3)
+        expect { oystercard.top_up(3) }.to change { oystercard.balance }.by(3)
+      end 
+      it 'raises an error if the balance exceeds 90' do
+        oystercard = Oystercard.new
+        max_balance = Oystercard::MAX_BALANCE
+        oystercard.top_up(max_balance)
+        expect { oystercard.top_up(1) }.to raise_error 'exceeded balance'
+      end 
+    end 
+ end 
